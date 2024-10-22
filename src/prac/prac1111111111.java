69bf01fb33851f24153981ca532cdcaa1878f177
PCJ-93 <pw2900@naver.com>
2024-10-22 10:21:44 +0900

    로직만들기연습

---
+++ src/prac/prac1111111111.java
@@ -0,0 +1,56 @@
+package prac;
+
+import java.util.Scanner;
+
+public class prac1111111111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("------------------------------");
+		System.out.println("1.예금 : 2.출금 : 3.잔고 : 4.종료");
+		System.out.println("------------------------------");
+		System.out.print("숫자입력 : ");
+		int selectNum = scanner.nextInt();
+		
+		while(true) {
+			System.out.println("------------------------------");
+			System.out.println("1.예금 : 2.출금 : 3.잔고 : 4.종료");
+			System.out.println("------------------------------");
+			if(selectNum==1) {
+				System.out.println("선택>1");
+				System.out.println("예금액>10000");
+				break;
+			}
+			if (selectNum==2) {
+				System.out.println("선택>2");
+				System.out.println("출금액>2000");
+				break;
+			}
+			if (selectNum==3) {
+				System.out.println("선택>3");
+				System.out.println("잔고>8000");
+				break;
+			}
+			if(selectNum==4) {
+				System.out.println("선택>4");
+				System.out.println("프로그램 종료");
+				break;
+			} else {
+				System.out.println("잘못입력");
+			}
+			System.out.print("숫자입력 : ");
+			selectNum = scanner.nextInt();		
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
