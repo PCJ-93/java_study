6e9edc80ba269d893c86a6474594d3246bb9ffbf
PCJ-93 <pw2900@naver.com>
2024-10-28 10:28:13 +0900

    2차원 배열 연습

---
+++ src/prac/Prac12.java
@@ -0,0 +1,128 @@
+package prac;
+
+public class Prac12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int i;
+//		int j;
+//		int num = 0;
+//		
+//		int numArr[][] = new int[4][4];
+//		
+//		for(i=0; i<numArr.length; i++) {
+//			for(j=0; j<numArr.length; j++) {
+//				numArr[i][j] = num+1;
+//				num++;
+//				System.out.print(numArr[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		//SOL
+//		int num = 1;
+//		int[][] arr = new int[4][4];                   
+//		for(int i=0; i<4; i++) {
+//			for(int j=0; j<4; j++) {
+//				arr[i][j] = num;
+//				num++;
+//				System.out.printf("%3d", arr[i][j]);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		int i;
+//		int j;
+//		int num = 0;
+//		
+//		int numArr[][] = new int[4][4];
+//		
+//		for(i=0; i<numArr.length; i++) {
+//			for(j=0; j<numArr.length; j++) {
+//				numArr[i][j] = num+1;
+//				num = num+4;
+//				if(i==1) {
+//					num=2;
+//				}
+//				System.out.print(numArr[i][j] + " ");             //못풀음@@@
+//			}
+//			System.out.println();
+//		}
+		
+		//SOL
+//		int numArr[][] = new int[4][4];
+//		int num = 1;
+//		for(int i=0; i<4; i++) {
+//			for(int j=0; j<4; j++) {
+//				numArr[j][i] = num;
+//				num++;
+//			}
+//		}
+//		for(int i=0; i<4; i++) {
+//			for(int j=0; j<4; j++) {
+//			System.out.print(numArr[i][j] + " ");
+//			}
+//				System.out.println();
+//		}
+		
+
+//		int i;
+//		int j;
+//		int num = 17;
+//		
+//		int numArr[][] = new int[4][4];
+//		
+//		for(i=0; i<numArr.length; i++) {
+//			for(j=0; j<numArr.length; j++) {
+//				numArr[i][j] = num-1;
+//				num--;
+//				System.out.print(numArr[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		//SOL
+//		int numArr[][] = new int[4][4];
+//		int num = 1;
+//		for(int i=3; i>=0; i--) {
+//			for(int j=3; j>=0; j--) {
+//				numArr[i][j] = num;
+//				num++;
+//			}
+//		}
+//		for(int i=3; i>=0; i--) {
+//			for(int j=3; j>=0; j--) {
+//				System.out.print(numArr[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+			
+			
+		//SOL
+//		int numArr[][] = new int[4][4];
+//		int num = 16;
+//		for(int i=0; i<4; i++) {
+//			for(int j=0; j<4; j++) {
+//				numArr[j][i] = num;
+//				num--;
+//			}
+//		}
+//		for(int i=0; i<4; i++) {
+//			for(int j=0; j<4; j++) {
+//			System.out.print(numArr[i][j] + " ");
+//			}
+//				System.out.println();
+//		}
+			
+		
+		
+		
+		
+		}
+
+	}
+
+
