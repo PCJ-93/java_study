1e4f8a05ba07afc9dabacde2174ebaabf139dcf3
PCJ-93 <pw2900@naver.com>
2024-12-17 16:47:17 +0900

    클래스 감잡기 연습문제

--- java_study/src/prac/Elevator.java
+++ java_study/src/prac/Elevator.java
@@ -6,18 +6,20 @@ public class Elevator {
 		int a = 13;
 		int b = 7;
 		int c = 10;
+		int d = 23;
 // 결과 출력
 		System.out.printf("%d층 -> %s 엘레베이터\n", a, guide(a));
 		System.out.printf("%d층 -> %s 엘레베이터\n", b, guide(b));
 		System.out.printf("%d층 -> %s 엘레베이터\n", c, guide(c));
+		System.out.printf("%d층 -> %s 엘레베이터\n", d, guide(d));
 	}
 
 	public static String guide(int floor) {
 		/* 메소드를 완성하시오. */
 		if(floor<11) {
 			return "저층";
-		}else {
+		}else if(floor>=11 && floor<=20){
 			return "고층";
-		}
+		}else {return "없는 층 입니다.";}
 	}
 }
